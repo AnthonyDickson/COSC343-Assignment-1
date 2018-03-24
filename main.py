6f20a026be070ebec46201ecef2e052fb5d200c7
Wilson Low <wilson@dn-wlan-128-250.sns.otago.ac.nz>
2018-03-24 16:35:43 +1300

    end of day commit sat 24 March

--- main.py
+++ main.py
@@ -35,10 +35,8 @@ class RobotController:
 
     def is_white(self, value):
         """Check if the given value corresponds to a white tile.
-
         Args:
             value (int): A value read from the robot's colour sensor.
-
         Returns:
             bool: True if the given value corresponds to white, False otherwise
         """
@@ -46,10 +44,8 @@ class RobotController:
 
     def is_black(self, value):
         """Check if the given value corresponds to a black tile.
-
         Args:
             value (int): A value read from the robot's colour sensor.
-
         Returns:
             bool: True if the given value corresponds to black, False otherwise
         """
@@ -57,7 +53,6 @@ class RobotController:
 
     def move_to_rel(self, degrees, speed=360):
         """Move the robot forward a certain distance.
-
         Args:
             degrees (int): How many degrees to turn the robot's wheels.
             speed (int): How fast to run the motors.
@@ -70,7 +65,6 @@ class RobotController:
     def move_for_tiles(self, num_tiles, speed=360):
         """Move the robot forward in a straight line for a number of
         black tiles.
-
         Args:
             num_tiles (int): How many squares to return.
             speed (int): How fast to run the motors.
@@ -79,6 +73,7 @@ class RobotController:
         n = 0  # The number of black tiles counted.
         direction = 1  # The direction to correct the path.
         num_other = 0
+        num_black = 0
         can_count = True
 
         self.mLeft.run_forever(speed_sp=speed)
@@ -93,11 +88,11 @@ class RobotController:
             else:
                 num_other = 0
 
-            if num_other > 2:
+            if num_other > 3:
                 self.mLeft.stop()
                 self.mRight.stop()
 
-                direction = self._correct_path(direction, 80)
+                direction = self._correct_path(-direction, 80)
                 self.rotate(5 * -direction)
 
                 prev_val = 0
@@ -110,8 +105,12 @@ class RobotController:
 
             # If the robot was over something black and now it is over
             # something white:
-            if self.is_black(prev_val) and self.is_white(curr_val) and can_count:
+            if self.is_black(curr_val):
+                num_black += 1
+
+            if self.is_black(prev_val) and self.is_white(curr_val) and can_count and num_black > 1:
                 n += 1
+                num_black = 0
                 can_count = False
                 Sound.speak(str(n))
 
@@ -130,11 +129,9 @@ class RobotController:
 
     def _correct_path(self, direction, speed=360):
         """Correct the robot's heading.
-
         Args:
             direction (int): The direction to try first (-1 for left, 1 for right).
             speed (int): How fast to turn while trying to correct.
-
         Returns:
               int: The direction the robot turned to correct it's heading.
         """
@@ -163,13 +160,13 @@ class RobotController:
         # Reset to starting direction.
         self.rotate(-90 * direction, speed)
         # Backup
-        self.move_to_rel(-180)
+        self.move_to_rel(-100)
+        self.rotate(45 * direction, 180)
 
         return self._correct_path(direction * -1)
 
     def rotate(self, degrees, speed=360):
         """Rotate the robot either clockwise or counter-clockwise.
-
         Args:
             degrees (int): How many degrees to turn the robot. Use a negative
                 number to rotate the robot counter-clockwise.
@@ -185,7 +182,6 @@ class RobotController:
     def move_until_touching(self, speed=360):
         """Move the robot until it is touching something with it's
         touch sensor.
-
         Args:
             speed (int): How fast to run the motors.
         """
@@ -204,12 +200,10 @@ class RobotController:
 
     def find_tower(self, degrees=180, threshold=800):
         """Find the tower, align the robot with it and find the distance to it.
-
         Args:
             degrees (int): How many degrees to check when searching for the tower.
             threshold (int): The maximum distance (mm) to detect the tower,
                 anything further away than this value will be ignored.
-
         Returns:
             int: The distance to the tower. Returns sys.maxsize if nothing was
             found within the threshold distance.
@@ -234,48 +228,55 @@ class RobotController:
             # away, break
             if was_found and distance > prev_distance:
                 # Turn back a bit to correct the angle.
-                self.rotate(-10, 180)
+                # self.rotate(-5, 180)
                 break
 
             if distance <= threshold:
                 was_found = True
                 prev_distance = distance
 
-            time.sleep(0.1)
+            time.sleep(0.05)
 
         self.mLeft.stop()
         self.mRight.stop()
 
         # If nothing was found or the thing found was not within range.
-        if not was_found or distance > threshold:
+        if not was_found:
             self.rotate(int(-degrees / 2), speed=180)
             return sys.maxsize
 
-        return distance
+        return prev_distance
 
 
 def main():
     rbt = RobotController()
-    # rbt.move_to_rel(degrees=320)
-    # rbt.rotate(degrees=90)
+    rbt.move_to_rel(degrees=320)
+    rbt.rotate(degrees=90)
     rbt.move_for_tiles(num_tiles=15, speed=180)
-    # rbt.rotate(degrees=90)
-    # rbt.move_to_rel(degrees=360 * 10, speed=720)
+    rbt.rotate(degrees=90)
+    rbt.move_to_rel(degrees=360 * 11, speed=720)
     # # Offset the rotation due to the robot veering to the left.
     # rbt.rotate(degrees=5, speed=180)
-    # distance = rbt.find_tower(threshold=800)
-    #
-    # # While nothing is in range...
-    # while distance == sys.maxsize:
-    #     # Move forward a bit
-    #     rbt.move_to_rel(degrees=360, speed=180)
-    #     # Try find the tower again
-    #     distance = rbt.find_tower(degrees=260, threshold=800)
-    #
-    # while distance > 300:
-    #     rbt.move_to_rel(degrees=360 * (distance/150), speed=360)
-    #     distance = rbt.find_tower(degrees=90, speed=180)
-    # rbt.move_to_rel(degrees=360 * (distance / 90), speed=900)  # Ramming speed!
+
+    distance = rbt.find_tower(threshold=1200)
+
+    while distance == sys.maxsize:
+        rbt.move_to_rel(degrees=360)
+        distance = rbt.find_tower(threshold=1200)
+
+    rbt.move_to_rel(degrees=360 * (distance / 250))
+
+
+    distance = rbt.find_tower(threshold=400)
+
+    # While nothing is in range...
+    while distance == sys.maxsize:
+        # Move forward a bit
+        rbt.move_to_rel(degrees=360, speed=180)
+        # Try find the tower again
+        distance = rbt.find_tower(degrees=260, threshold=400)
+
+    rbt.move_to_rel(degrees=360 * (distance / 70), speed=900)  # Ramming speed!
     rbt.beep()
 
 
